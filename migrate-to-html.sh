b249e06822d38e9b102ca9c0db703c7ccbc90f32
bob omen 2 <jsbachvu@gmail.com>
2025-07-12 01:41:15 +0200

    fix album

--- migrate-to-html.sh
+++ migrate-to-html.sh
@@ -1,7 +1,8 @@
-for p in $(du -a collections | grep .mp3)
+du -a collections | grep .mp3 | while IFS="" read -r p || [ -n "$p" ] 
 do
+	#echo ${p#*	}
 	f_mp3=${p#*	};
 	f_html=${f_mp3/%.mp3/.html};
 	echo "renaming html ${f_mp3} ..."
-	mv ./${f_mp3} ./${f_html};
+	mv "./${f_mp3}" "./${f_html}";
 done
